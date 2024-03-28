14c8bccaf79ab98f6934687b8c8e7219918f0e1c
mKomorek <marcin.komorek@protonmail.com>
2024-03-28 16:13:35 +0100

    change to interface lib with UTs

--- src/include/linkit/signal.hpp
+++ src/include/linkit/signal.hpp
@@ -1,13 +1,29 @@
 #pragma once
 
 #include <functional>
+#include <type_traits>
+#include <utility>
+#include <vector>
 
 namespace linkit {
 
 template <typename... Args> class Signal {
 public:
-  template <typename Slot> auto connect(Slot &&slot) -> void;
-  auto emit(Args &&...args) const -> void;
+  Signal() = default;
+  Signal(const Signal &) = delete;
+  Signal(Signal &&) = delete;
+
+  template <typename Slot,
+            typename = std::enable_if_t<!std::is_same_v<Slot, Signal<Args...>>>>
+  auto connect(Slot &&slot) -> void {
+    this->connections_.emplace_back(std::forward<Slot>(slot));
+  }
+
+  auto emit(Args &&...args) const -> void {
+    for (const auto &connection : this->connections_) {
+      connection(std::forward<Args>(args)...);
+    }
+  }
 
 private:
   std::vector<std::function<void(Args...)>> connections_;
--- tests/UT_linkit/signalTests.cpp
+++ tests/UT_linkit/signalTests.cpp
@@ -4,12 +4,48 @@
 
 namespace linkit {
 
-class SignalTest : public ::testing::Test {
-protected:
-  Signal<int> signal;
-};
+TEST(SignalTest, singleConnection) {
+  auto value = int{0};
 
-// Test connecting slots
-TEST_F(SignalTest, connecttt) { ASSERT_EQ(true, true); }
+  auto valueChange = Signal<int>{};
+  valueChange.connect([&value](int x) { value = x; });
+
+  valueChange.emit(997);
+
+  EXPECT_EQ(value, 997);
+}
+
+TEST(SignalTest, multipleConnections) {
+  auto count = int{0};
+
+  auto signal = Signal<int>{};
+  signal.connect([&count](const auto x) { count += x; });
+  signal.connect([&count](const auto x) { count += x; });
+
+  signal.emit(10);
+
+  EXPECT_EQ(count, 20);
+}
+
+TEST(SignalTest, connectDifferentTypesWithSingleSignal) {
+  auto intArg = int{0};
+  auto floatArg = float{0};
+  auto stringArg = std::string{};
+
+  auto differentTypesSignal = Signal<int, float, std::string>{};
+  differentTypesSignal.connect([&intArg, &floatArg, &stringArg](
+                                   const int intValue, const float floatValue,
+                                   const std::string &stringValue) {
+    intArg = intValue;
+    floatArg = floatValue;
+    stringArg = stringValue;
+  });
+
+  differentTypesSignal.emit(5, 3.14f, "Hello");
+
+  EXPECT_EQ(intArg, 5);
+  EXPECT_FLOAT_EQ(floatArg, 3.14f);
+  EXPECT_EQ(stringArg, "Hello");
+}
 
 } // namespace linkit
